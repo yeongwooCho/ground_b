794900d14747c077a0d392684b555f67aceb037f
yeongwoo <jyy0223@naver.com>
2024-07-26 20:24:46 +0900

    [feat] manufacturing image 추가

--- lib/common/const/image_path.dart
+++ lib/common/const/image_path.dart
@@ -1,24 +1,30 @@
 class ImagePath {
-  /// logo, icon
+  // logo, icon
   static const appIcon = "asset/img/logo/app_icon.png";
   static const logo = "asset/img/logo/logo.png";
 
-  /// social_login button
+  // social_login button
   static const appleIcon = "asset/img/social_login/button/apple.png";
   static const googleIcon = "asset/img/social_login/button/google.png";
   static const kakaoIcon = "asset/img/social_login/button/kakao.png";
   static const naverIcon = "asset/img/social_login/button/naver.png";
 
-  /// social_login screen
+  // social_login screen
   static const appleScreen = "asset/img/social_login/screen/apple.png";
   static const googleScreen = "asset/img/social_login/screen/google.png";
   static const kakaoScreen = "asset/img/social_login/screen/kakao.png";
   static const naverScreen = "asset/img/social_login/screen/naver.png";
 
-  /// user
+  // user
   static const user = "asset/img/user/user.png";
 
-  /// banner
+  // banner
   static const homeBanner = "asset/img/banner/home_banner.png";
   static const manufacturingBanner = "asset/img/banner/manufacturing_banner.png";
+
+  // manufacturing
+  static const manufacturingDirectory = "asset/img/manufacturing/";
+  // product
+  static const productDirectory = "asset/img/product/";
+
 }
