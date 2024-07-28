0812403e07d4a9b88345df9113626783acb2d297
yeongwoo <jyy0223@naver.com>
2024-07-28 15:16:45 +0900

    [feat] 생산 페이지 - 생산요청 현황 컨테이너: padding 값 수정

--- lib/manufacturing/component/manufacturing_request_container.dart
+++ lib/manufacturing/component/manufacturing_request_container.dart
@@ -80,13 +80,13 @@ class _RequestButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(8.0),
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
                 title,
-                style: MyTextStyle.bodyBold,
+                style: MyTextStyle.bodyMedium,
               ),
               Text(
                 count.toString(),
